bdb2c03831db90bc9b433093e5aa869691b26d71
Maxwell Phillips <maxwell20153@gmail.com>
2018-11-28 11:08:57 +0100

    Removed default JavaFX files

--- Application/src/sample/Main.java
+++ Application/src/sample/Main.java
@@ -11,19 +11,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /*
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-        Stage newStage = new Stage();
-        newStage.setTitle("Another stage");
-        Parent anotherRoot = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        newStage.setScene(new Scene(anotherRoot, 200, 275));
-        newStage.show();
-
+        */
     }
-
 
     public static void main(String[] args) {
         launch(args);
